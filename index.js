e2173eee328d223e1c286899495039b8a9ac4888
Hayley van Waas <hayleyavw@gmail.com>
2021-09-07 20:16:37 +0800

    WIP Used API rather than dummy data.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { peopleData } from './fixtures/people.js'
+import peopleRouter from './routes/people.js'
 
 const app = express();
 app.use(express.static('static'))
@@ -10,26 +10,7 @@ app.listen(3000, () => {
     console.log('Server started!')
 });
 
-app.get('/', (req, res) => {
-    // res.send('Hello, world!')
-    console.log(peopleData);
-    res.render('index', { people: peopleData });
-});
 
-app.get('/profile/:id', (req, res) => {
-    // console.log(req)
-    let personId = req.params.id;
-    let person;
-    peopleData.forEach((personData) => {
-        // console.log(personData.id, personId)
-        if (personData.id == personId) {
-            person = personData;
-            // return false;
-        }
-    })
-    console.log(person);
-    res.render('profile', { person: person});
-})
-
+app.use('/', peopleRouter)
 
 // 127.0.0.1:3000
