6e4bc4069ddb01dd05351d3cacc9e3a4c964fd4c
Hayley van Waas <hayleyavw@gmail.com>
2021-09-14 19:35:31 +0800

    Created form for submitting new profiles.

--- api/people.js
+++ api/people.js
@@ -23,12 +23,29 @@ export const fetchPeople = async function (name) {
 }
 
 
-export const fetchPerson = function (id) {
-    let person;
-    peopleData.forEach((personData) => {
-        if (personData.id == id) {
-            person = personData;
+export const fetchPerson = async function (id) {
+    const url = `${API_URL}people/${id}`;
+    console.log(url);
+    const response = await fetch(url, {
+        headers: {
+            Authorization: `Token ${API_KEY}`
+        },
+    });
+    if (response.status == 401) {
+        console.log('not authorized.')
+        return;
+    }
+    return response.json();
+}
+
+
+export const createPerson = async function(formData) {
+    const response = await fetch(`${API_URL}people`, {
+        method: 'POST',
+        body: formData,
+        headers: {
+            Authorization: `Token ${API_KEY}`,
         }
     });
-    return person;
+    return response.json();
 }
